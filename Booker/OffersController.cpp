f3b199d12372cf0d6dbafedffc1625fdd9402d27
cashtann <switalamaciej5@gmail.com>
2024-09-22 11:08:03 +0200

    Updated readme

--- Booker/OffersController.cpp
+++ Booker/OffersController.cpp
@@ -11,26 +11,41 @@ OffersController::OffersController(QObject *parent)
 {
     ElementModel* element1 = new ElementModel(this);
     //element->name = "test";
-    element1->addElement("Cracow", "Meet pigeons", "Poland", Utilities::loremIpsum, QUrl("qrc:/res/assets/images/preview_cracow.jpg"), 12, 5.0);
-    element1->addElement("Moscow", "Taste the strongest beverages", "Russia", Utilities::loremIpsum, QUrl("qrc:/res/assets/images/preview_moscow.jpg"), 34, 2.3);
-    element1->addElement("London", "Better learn to run", "Great Britain", Utilities::loremIpsum, QUrl("qrc:/res/assets/images/preview_london.jpg"), 56, 4.5);
-    element1->addElement("Copenhagen", "Not enough bikes?", "Denmark", Utilities::loremIpsum, QUrl("qrc:/res/assets/images/preview_copenhagen.jpg"), 56, 3.2);
+    element1->addElement("Great place", "", "Poland", Utilities::loremIpsum, QUrl("qrc:/res/assets/images/home1.jpg"), 12, 5.0);
+    element1->addElement("Nice House", "", "Russia", Utilities::loremIpsum, QUrl("qrc:/res/assets/images/home2.jpg"), 34, 2.3);
+    element1->addElement("Good for everyday", "", "Great Britain", Utilities::loremIpsum, QUrl("qrc:/res/assets/images/home3.jpg"), 23545, 4.5);
+    element1->addElement("Best rest", "", "Denmark", Utilities::loremIpsum, QUrl("qrc:/res/assets/images/home4.jpg"), 56, 3.2);
+
+    ElementModel* element2 = new ElementModel(this);
+    //element->name = "test";
+    element2->addElement("Great place", "", "Poland", Utilities::loremIpsum, QUrl("qrc:/res/assets/images/home1.jpg"), 12, 5.0);
+    element2->addElement("Nice House", "", "Russia", Utilities::loremIpsum, QUrl("qrc:/res/assets/images/home2.jpg"), 34, 2.3);
+    element2->addElement("Good for everyday", "", "Great Britain", Utilities::loremIpsum, QUrl("qrc:/res/assets/images/home3.jpg"), 23545, 4.5);
+    element2->addElement("Best rest", "", "Denmark", Utilities::loremIpsum, QUrl("qrc:/res/assets/images/home4.jpg"), 56, 3.2);
 
     LocationModel* location1 = new LocationModel(this);
     location1->addLocation("Poland", "The land of blooming onions", QUrl("qrc:/res/assets/images/preview_warsaw.jpg"), element1);
-    location1->addLocation("Poland", "The land of blooming onions", QUrl("qrc:/res/assets/images/preview_warsaw.jpg"), element1);
-    location1->addLocation("Poland", "The land of blooming onions", QUrl("qrc:/res/assets/images/preview_warsaw.jpg"), element1);
-    location1->addLocation("Poland", "The land of blooming onions", QUrl("qrc:/res/assets/images/preview_warsaw.jpg"), element1);
-    location1->addLocation("Poland", "The land of blooming onions", QUrl("qrc:/res/assets/images/preview_warsaw.jpg"), element1);
+    location1->addLocation("USA", "Feel the liberty", QUrl("qrc:/res/assets/images/preview_usa.jpg"), element1);
+    location1->addLocation("Russia", "Good beverages", QUrl("qrc:/res/assets/images/preview_moscow.jpg"), element1);
+    location1->addLocation("Copenhagen", "Watch out for bikes", QUrl("qrc:/res/assets/images/preview_copenhagen.jpg"), element1);
+    location1->addLocation("Japan", "Yakuza good good", QUrl("qrc:/res/assets/images/preview_tokyo.jpg"), element1);
 
-    addCategory("Europe",
-                "Explore Europe",
-                "Dive into beautiful and meaningful history of the old continent",
+    LocationModel* location2 = new LocationModel(this);
+    location2->addLocation("Las Vegas", "Gamble it all", QUrl("qrc:/res/assets/images/preview_las_vegas.jpg"), element2);
+    location2->addLocation("San Francisco", "Watch your step", QUrl("qrc:/res/assets/images/preview_usa.jpg"), element2);
+    location2->addLocation("Russia", "Good beverages", QUrl("qrc:/res/assets/images/preview_moscow.jpg"), element2);
+    location2->addLocation("Copenhagen", "Watch out for bikes", QUrl("qrc:/res/assets/images/preview_copenhagen.jpg"), element2);
+
+    addCategory("Major Cities",
+                "Major Cities",
+                //"Dive into beautiful and meaningful history of the old continent",
+                "Explore biggest cities, explore world",
                 location1);
-    ElementModel* element2 = new ElementModel(this);
-    element2->addElement("Cracow", "Meet pigeons", "Poland", Utilities::loremIpsum, QUrl("qrc:/res/assets/images/preview_cracow.jpg"), 12, 12);
-    element2->addElement("Copenhagen", "Not enough bikes?", "Denmark", Utilities::loremIpsum, QUrl("qrc:/res/assets/images/preview_copenhagen.jpg"), 56, 56);
-    addCategory("Name", "Header", "Description", location1);
+
+    addCategory("USA I guess",
+                "Freedom everywhere",
+                "Don't get shot",
+                location2);
 }
 
 int OffersController::rowCount(const QModelIndex &parent) const
--- README.md
+++ README.md
@@ -1,7 +1,22 @@
-# Booker
+# Booker - rental
 
-Copy of Booking.com.
+Inspired by Booking.com.
 
-*Not really for commercial use.*
+Made with Qt. Using QML and C++, compiles on Desktop and WebAssembly.
 
-Made for educational purposes only
+This rental has few categories:
+- Categories
+- Locations
+- Elements
+
+There is a functional cart system, you can add multiple elements to your cart without any problems.
+
+There are multiple pages you can navigate.
+
+### *Elements are just placeholders, they do not mean anything. Titles and headers may also not fit to theirs elements, they are placeholders!*
+
+![preview_mainpage](./preview_mainpage.png)
+![preview_elementlist](./preview_elementlist.png)
+![preview_elementlist2](./preview_elementlist2.png)
+![preview_element.png](./preview_element.png)
+![preview_cart.png](./preview_cart.png)
